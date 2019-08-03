8a4a2473d1a44d3dcf985348bfe6978225ee1de8
free_learn <matiaswebdev@gmail.com>
2019-08-03 13:36:34 -0300

    modified user table extructure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('nivel')->nullable();
+            $table->boolean('ativo')->nullable();
             $table->timestamps();
         });
     }
